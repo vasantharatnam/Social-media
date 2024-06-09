6c8d8eb5d1713d6ece44ce7188f18581a72b9aa0
RatanKalpaSai <97551433+RatanKalpaSai@users.noreply.github.com>
2024-06-09 14:13:27 +0530

    #10 Added new Host in comment

--- backend/utils/emailVerification.mjs
+++ backend/utils/emailVerification.mjs
@@ -11,6 +11,7 @@ const { AUTH_EMAIL, AUTH_PW, APP_URL } = process.env;
 
 
 let transporter = nodemailer.createTransport({
+    // host: "	smtp.office365.com", // hostname
     host: "smtp-mail.outlook.com", // hostname
     secureConnection: true, // TLS requires secureConnection to be false
     port: 587,
@@ -29,6 +30,7 @@ const sendVerificationMail = async (user, res) => {
     let _id;
     let email;
     let name;
+    console.log(AUTH_EMAIL, AUTH_PW, APP_URL);
     const q = query(collection(dbStore, "users"), orderBy("createdAt", "desc"), limit(1));
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
